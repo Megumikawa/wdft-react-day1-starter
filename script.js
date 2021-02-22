1a39599f45d91159f3b21821a6e7b453fdcc575e
Megumikawa <megumi19901016@gmail.com>
2021-02-22 16:33:16 +0100

    did in class

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
 // Once you link both React and ReactDOM libraries 
 // you will have access to two global variables `React` and `ReactDOM`
+
+// console.log(React)
+
+// console.log(ReactDOM)
+
+
+// let element = "Hello Ironhackers!"
+
+// let element = React.createElement('h1', null, "Hello Ironhackers!")
+
+function Header() {
+  return <div><h1>"Hello Ironhackers!"</h1></div>
+}
+
+function Footer() {
+  return <h5>"Made by No One</h5>
+}
+
+class MainSection extends React.Component {
+  
+  render(){
+    return <h3>My main section is here!!</h3>
+  }
+}
+
+
+
+let element = (
+  <div>
+    <Header />
+    <MainSection />
+    <Footer />
+  </div>
+  )
+
+
+
+ReactDOM.render(element, document.querySelector('#myApp'))
+
+/* same as ↑
+ReactDOM.render(<Header/>, document.querySelector('#myApp'))
+*/
